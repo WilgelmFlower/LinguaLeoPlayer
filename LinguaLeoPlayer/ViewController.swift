6a299feed59adaf9cbb64c556e967157999c954a
Mark Wilgelm <markujke@mail.ru>
2023-08-09 10:59:06 +0200

    Initial Commit

---
+++ LinguaLeoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinguaLeoPlayer
+//
+//  Created by Mark Wilhelm on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
